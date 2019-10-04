e76a47afb1f9ee07b5274c249189089a803d9c42
diturrizagac <diturrizagac@gmail.com>
2019-10-04 12:06:47 -0500

    first commit

---
+++ app/src/main/java/com/example/scoreregisterapp/SelectRoleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.scoreregisterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectRoleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_role)
+    }
+}
---
+++ app/src/main/java/com/example/scoreregisterapp/TeacherCoursesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.scoreregisterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TeacherCoursesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teacher_courses)
+    }
+}
